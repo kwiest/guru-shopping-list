0ea557b2434eee1d0109e8ecf240dad42fd23ce6
Kyle Wiest <kyle.wiest@bluebottlecoffee.com>
2023-12-15 13:01:22 -0800

    Initial commit

---
+++ test/guru-shopping-list.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GuruShoppingList from '../lib/guru-shopping-list-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/guru-shopping-list-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GuruShoppingList.GuruShoppingListStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
