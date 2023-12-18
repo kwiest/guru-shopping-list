d704154bcdd73180db8086c075b7cf9b1b24067e
Kyle Wiest <kyle.wiest@bluebottlecoffee.com>
2023-12-18 10:18:34 -0800

    Revert "feat: return created values"
    This reverts commit 107477f44f3de9fd8f97a6db7e2dc7e3589200cd.
    
    Only "NONE" or "ALL_OLD" are allowed for PutItem commands.

--- functions/v1/create-shopping-lists/insert-list.ts
+++ functions/v1/create-shopping-lists/insert-list.ts
@@ -37,7 +37,6 @@ export async function insertList({
       type: { S: "shopping_list" },
       created_at: { N: createdAt.getTime().toString() },
     },
-    ReturnValues: "ALL_NEW",
     ConditionExpression: "attribute_not_exists(SK)",
   });
 
